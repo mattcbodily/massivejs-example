16a58c1ad2c8422777a79a49f4f07e314490a317
Matt Bodily <mattcbodily@gmail.com>
2019-10-09 14:04:10 -0600

    added comments to the index.js file for clarity

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express'),
       massive = require('massive'),
       ctrl = require('./controller'),
+      //destructuring our variables from our .env file from the process.env object
       {SERVER_PORT, CONNECTION_STRING} = process.env,
       app = express();
 
 app.use(express.json());
 
+//massive takes the connection string of our database to connect to the database
 massive(CONNECTION_STRING).then(db => {
     app.set('db', db);
     console.log('DB connected');
@@ -17,5 +19,6 @@ app.post('/api/user', ctrl.postUser);
 app.put('/api/user/:id', ctrl.updateUser);
 app.delete('/api/user/:id', ctrl.deleteUser);
 
+//SERVER_PORT comes from the .env file
 const port = SERVER_PORT;
 app.listen(port, () => console.log(`Server running on ${port}`));
